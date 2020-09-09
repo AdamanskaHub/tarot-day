648e7b93f9282088cb8683737ce086de16612a98
Emilie <adamanska@gmail.com>
2020-09-09 16:57:14 +0200

    bubble with dialogue

--- src/Card.js
+++ src/Card.js
@@ -6,29 +6,30 @@ function Card(props) {
   const cardList = [
     {
       cardName: 'Name of the card',
-      cardPicture: '/img/backimg.png',
+      cardPicture: '/img/soleil.png',
       dialogue: ['option a', 'option b', 'option c'],
     },
     {
       cardName: 'Name of the card 2',
-      cardPicture: '/img/backimg.png',
+      cardPicture: '/img/lune.png',
       dialogue: ['option 0', 'option 1', 'option 2'],
     },
   ];
 
-  // console.log(articles[1].dialogue[1])
-
-  const [dialogue, setDialogue] = useState(0);
+  const [dialogue, setDialogue] = useState('default text');
   const [card, setCard] = useState(0);
+  const [cardImg, setCardImg] = useState('/img/backimg.png')
   const [clicked, setClicked] = useState(false);
 
   // const { anecdotes } = props;
 
   const cardDisplay = () => {
+    console.log("ACTIVATED THE CLICK")
     setClicked(true);
     if (!clicked) {
       setCard([Math.floor(Math.random() * 2)])
       setDialogue([Math.floor(Math.random() * 3)])
+      setCardImg(cardList[card].cardPicture)
     }
   }
 
@@ -38,12 +39,23 @@ function Card(props) {
   return (
     <div className="main">
 
-      <img src='/img/backimg.png' className="card" alt="tarot card" />
-      <p>Text the guy says, card = {card}, dialogue = {dialogue}</p>
-      <p>You clicked and random value from array: {dialogue}</p>
+      <img src={cardImg} className="card" alt="tarot card" />
+      <p>Text the guy says, card = {card}, dialogue = {dialogue}, img = {cardImg}</p>
       <button onClick={cardDisplay}>
         Click me
       </button>
+
+      <div className="bubble">
+
+        <div className="bubble-border" />
+        <div className="bubble-main">
+          {dialogue}
+        </div>
+        <div className="bubble-border" />
+
+
+      </div>
+
     </div>
 
   );
